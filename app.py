e9e656df84c63413cd9a1172559f16e7c811bd4e
IdrisFallout <waithakasam2017@gmail.com>
2023-05-22 21:32:05 +0300

    add POSTGRES database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import secrets
-
+import os
+from flask_sqlalchemy import SQLAlchemy
+import uuid
 from flask import Flask, url_for, redirect, request, abort
 from versions import v1, v2
 
@@ -8,15 +9,50 @@ app = Flask(__name__)
 app.register_blueprint(v1.api_v1, url_prefix='/api/v1')
 app.register_blueprint(v2.api_v2, url_prefix='/api/v2')
 
-app.secret_key = secrets.token_hex(16)  # Generate a random 32-character hexadecimal string
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+app.app_context().push()
 
 
-def generate_api_key():
-    api_key = secrets.token_hex(16)  # Generate a random 32-character hexadecimal string
-    return api_key
+class DeviceModel(db.Model):
+    __tablename__ = 'devices'
 
+    id = db.Column(db.Integer, primary_key=True)
+    device_name = db.Column(db.String(80))
+    device_key = db.Column(db.String(80))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('UserModel', back_populates="devices")
 
-print(generate_api_key())
+    def __init__(self, device_name, user_id, device_key=None):
+        self.device_name = device_name
+        self.user_id = user_id
+        self.device_key = device_key or uuid.uuid4().hex
+
+    def json(self):
+        return {
+            'device_name': self.device_name,
+            'device_key': self.device_key,
+            'user_id': self.user_id
+        }
+
+    @classmethod
+    def find_by_name(cls, device_name):
+        return cls.query.filter_by(device_name=device_name).first()
+
+    @classmethod
+    def find_by_device_key(cls, device_key):
+        return cls.query.filter_by(device_key=device_key).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False, host='0.0.0.0', port=5000)
