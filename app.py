e9e213b3711d3ad6720a3f50241a62cefb8e23f2
IdrisFallout <waithakasam2017@gmail.com>
2023-05-23 21:59:28 +0300

    api-key update

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 from versions import v1, v2
+import secrets
 
 app = Flask(__name__)
 # Register the blueprints from version scripts
@@ -15,6 +16,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 app.app_context().push()
+
+
+# generate an API key
+@app.route('/generate-api-key', methods=['GET'])
+def generate_api_key():
+    api_key = secrets.token_hex(16)
+    print(api_key)
+    # print the IP address of the client
+    print("CLIENT: " + request.remote_addr)
+    # Perform further processing with the generated API key
+    return 'API Key generated.'
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
--- versions/v1.py
+++ versions/v1.py
@@ -1,6 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort
 
 api_v1 = Blueprint('api_v1', __name__)
+
+
+# require API key for all routes in this blueprint
+@api_v1.before_request
+def before_request():
+    # get the API key from the query string
+    api_key = request.headers.get('X-API-Key')
+
+    # if the API key is missing or invalid, return an error
+    if not api_key or api_key != 'eiWee8ep9due4deeshoa8Peichai8Eih':
+        abort(401)
 
 
 @api_v1.route('/resource', methods=['GET'])
